80a4809b176ee7f92130ec900f1fdb3afc762bc2
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-02-25 19:01:14 -0800

    updated code to use an app context for updating values in db

--- test_user_model.py
+++ test_user_model.py
@@ -25,8 +25,8 @@ from app import app
 # Create our tables (we do this here, so we only create the tables
 # once for all tests --- in each test, we'll delete the data
 # and create fresh new clean test data
-
-db.create_all()
+with app.app_context():
+    db.create_all()
 
 
 class UserModelTestCase(TestCase):
@@ -34,24 +34,24 @@ class UserModelTestCase(TestCase):
 
     def setUp(self):
         """Create test client, add sample data."""
-
-        User.query.delete()
-        Message.query.delete()
-        Follows.query.delete()
+        with app.app_context():
+            User.query.delete()
+            Message.query.delete()
+            Follows.query.delete()
 
         self.client = app.test_client()
-
+    
     def test_user_model(self):
         """Does basic model work?"""
 
-        u = User(
-            email="test@test.com",
-            username="testuser",
-            password="HASHED_PASSWORD"
-        )
-
-        db.session.add(u)
-        db.session.commit()
+        with app.app_context():
+            u = User(
+                email="test@test.com",
+                username="testuser",
+                password="HASHED_PASSWORD"
+            )
+            db.session.add(u)
+            db.session.commit()
 
         # User should have no messages & no followers
         self.assertEqual(len(u.messages), 0)
