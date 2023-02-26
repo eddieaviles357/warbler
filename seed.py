83cdcaed101e2bd935d988dbbd40076ca590b2bb
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-02-25 18:50:52 -0800

    put code from seed file into its own function called init_db()

--- seed.py
+++ seed.py
@@ -4,28 +4,18 @@ from csv import DictReader
 from app import db
 from models import User, Message, Follows
 
+def init_db():
+    """ Seed db """
+    db.drop_all()
+    db.create_all()
 
-db.drop_all()
-db.create_all()
+    with open('generator/users.csv') as users:
+        db.session.bulk_insert_mappings(User, DictReader(users))
 
-with open('generator/users.csv') as users:
-    db.session.bulk_insert_mappings(User, DictReader(users))
+    with open('generator/messages.csv') as messages:
+        db.session.bulk_insert_mappings(Message, DictReader(messages))
 
-with open('generator/messages.csv') as messages:
-    db.session.bulk_insert_mappings(Message, DictReader(messages))
+    with open('generator/follows.csv') as follows:
+        db.session.bulk_insert_mappings(Follows, DictReader(follows))
 
-with open('generator/follows.csv') as follows:
-    db.session.bulk_insert_mappings(Follows, DictReader(follows))
-
-db.session.commit()
-
-    # db.drop_all()
-    # db.create_all()
-    # with open('generator/users.csv') as users:
-    #     db.session.bulk_insert_mappings(User, DictReader(users))
-    # with open('generator/messages.csv') as messages:
-    #     db.session.bulk_insert_mappings(Message, DictReader(messages))
-
-    # with open('generator/follows.csv') as follows:
-    #     db.session.bulk_insert_mappings(Follows, DictReader(follows))
-    # db.session.commit()
+    db.session.commit()
